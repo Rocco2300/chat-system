f9fd49e4e3d88554884abd1fb3abaeec9158a52c
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-02-03 15:57:41 +0200

    build the client

--- client/main.c
+++ client/main.c
@@ -1,7 +1,50 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+const uint32_t PORT = 8083;
 
 int main()
 {
-    printf("Hello world from client!\n");
+    int sock = 0, valread, clientfd;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client!";
+    char buffer[1024] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("Socket error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    clientfd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (clientfd < 0)
+    {
+        perror("Connection failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    valread = read(sock, buffer, 1024);
+    if (valread)
+        printf("%s\n", buffer);
+
+    close(clientfd);
     return 0;
 }
